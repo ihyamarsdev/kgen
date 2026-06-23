c792fbd33db7b46a0e91f78a677e34c60fc16341
ihyamars <ihyamars@gmail.com>
2026-06-23 10:52:27 +0700

    chore: bump version to v0.2.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -15,7 +15,7 @@ const (
 // it at build time via -ldflags:
 //
 //	go build -ldflags "-X kgen/internal/version.Version=v0.2.0" -o kgen main.go
-var Version = "v0.1.9"
+var Version = "v0.2.0"
 
 // RepoURL returns the canonical GitHub URL for the project.
 func RepoURL() string {
